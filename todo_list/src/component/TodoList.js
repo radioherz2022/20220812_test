77d889a569aed80c221696d16f90fa8174e2b191
radioherz2022 <jes1025k@gmail.com>
2022-08-14 05:44:40 +0900

    useReducer로 변경

--- todo_list/src/component/TodoList.js
+++ todo_list/src/component/TodoList.js
@@ -5,8 +5,16 @@ export default function TodoList({todoList, onToggle, onRemove}){
     return ( 
     <Block>
         {todoList.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} onRemove={onRemove}/>))}
+        <TodoItem 
+        key={todo.id} 
+        todo={todo} 
+        onToggle={onToggle} 
+        onRemove={onRemove}/>))}
     </Block>
+    // 아래 각 객체를 todo라는 이름으로 접근하게 될 것!
+    // 첫번째 객체인 {id:1, text:"오늘의 할 일", done:false}
+    // 두번째 객체인 {id:2, text:"투두리스트 시험 중", done:false}
+    // 
     )
 }
 
